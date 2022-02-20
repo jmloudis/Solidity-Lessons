7e83363e2f1d3e2ad5f18d60a75f359773ac63e7
"Jordan Loudis" <"16jloudis@gmail.com">
2022-02-19 22:26:06 -0500

    Added a new fully functional ERC20 token contract

--- contracts/NewApp/NewApp.sol
+++ contracts/NewApp/NewApp.sol
@@ -6,10 +6,13 @@ pragma solidity ^0.8.7;
 
 contract NewApp {
 
+    // 2nd address 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
     address public owner;
     address public sent;
     address public from;
     string private message;
+    string[] private messages;
 
     mapping(address => mapping(address => bool)) public isFriend;
 
@@ -20,14 +23,12 @@ contract NewApp {
     function sendMessage(address _to, string calldata _message) external {
         emit Message(msg.sender, _to, _message);
         message = _message;
-        from = msg.sender;
         sent = _to;
         isFriend[msg.sender][_to] = true;
     }
 
-    function ViewLastMessage() public payable returns (string memory) {
-        require(isFriend[from][sent] == true, "Error");
-
+    function ViewLastMessage() public view returns (string memory) {
+        require(sent == msg.sender);
         return message;
 
     }
